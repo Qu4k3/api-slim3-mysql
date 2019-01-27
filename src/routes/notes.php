7f41180918bc45b46d0f3461b4e8e0551dbea4c9
Qu4k3 <paul.guillamon@gmail.com>
2019-01-27 16:42:47 +0100

    j7sn

--- src/routes/notes.php
+++ src/routes/notes.php
@@ -4,7 +4,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 $app = new \Slim\App;
 
-// GET Todas las notes
+// GET all notes
 $app->get('/api/notes', function(Request $request, Response $response){
     $sql = "SELECT * FROM notes";
     try {
@@ -15,7 +15,7 @@ $app->get('/api/notes', function(Request $request, Response $response){
             $notes = $result->fetchAll(PDO::FETCH_OBJ);
             echo json_encode($notes);
         } else {
-            echo json_encode("No existen notes en la BBDD");
+            echo json_encode("Notes not found");
         }
         $result = null;
         $db = null;
@@ -24,7 +24,7 @@ $app->get('/api/notes', function(Request $request, Response $response){
     }
 });
 
-// GET notes por ID
+// GET notes by ID
 $app->get('/api/notes/{id}', function(Request $request, Response $response){
     $id_note = $request->getAttribute('id');
     $sql = "SELECT * FROM notes WHERE id = $id_note";
@@ -36,7 +36,7 @@ $app->get('/api/notes/{id}', function(Request $request, Response $response){
             $notes = $result->fetchAll(PDO::FETCH_OBJ);
             echo json_encode($notes);
         } else {
-            echo json_encode("No existen notes en la BBDD");
+            echo json_encode("No notes exist with this ID");
         }
         $result = null;
         $db = null;
@@ -45,7 +45,7 @@ $app->get('/api/notes/{id}', function(Request $request, Response $response){
     }
 });
 
-// POST Crear nueva note
+// POST create new note
 $app->post('/api/notes/new', function(Request $request, Response $response){
     $title = $request->getParam('title');
     $content = $request->getParam('content');
@@ -73,7 +73,36 @@ $app->post('/api/notes/new', function(Request $request, Response $response){
     }
 });
 
-// DELETE note
+// PUT edit note by ID
+$app->put('/api/notes/edit/{id}', function(Request $request, Response $response){
+    $id_note = $request->getAttribute('id');
+    $title = $request->getParam('title');
+    $content = $request->getParam('content');
+    $book = $request->getParam('book');
+    $user = $request->getParam('user');
+
+    $sql = "INSERT INTO notes SET title = :title, content = :content, book = :book, user = :user WHERE id = $id_note";
+    try {
+        $db = new db();
+        $db = $db->connectDB();
+        $result = $db->prepare($sql);
+
+        $result->bindParam(':title', $title);
+        $result->bindParam(':content', $content);
+        $result->bindParam(':book', $book);
+        $result->bindParam(':user', $user);
+
+        $result->execute();
+        echo json_encode("Note edited");
+
+        $result = null;
+        $db = null;
+    } catch(PDOException $e) {
+        echo '{"error" : {"text":'.$e->getMessage().'}';
+    }
+});
+
+// DELETE note by ID
 $app->delete('/api/notes/delete/{id}', function(Request $request, Response $response){
     $id_note = $request->getAttribute('id');
 
@@ -89,7 +118,7 @@ $app->delete('/api/notes/delete/{id}', function(Request $request, Response $resp
         if ($result->rowCount() > 0){
             echo json_encode("Note deleted");
         } else {
-            echo json_encode("Note doenst exist");
+            echo json_encode("Note doesn't exist");
         }
 
         $result = null;
